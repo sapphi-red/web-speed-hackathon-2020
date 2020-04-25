2c230a5c3c8806ab43df0e7497880f45e6484f02
sapphi-red <green@sapphi.red>
2020-04-25 23:00:54 +0900

    cache image

--- src/domains/blog_list/blog_list_actions.js
+++ src/domains/blog_list/blog_list_actions.js
@@ -3,9 +3,13 @@ import { fetch } from '../../foundation/gateway';
 export const ACTION_BLOG_LIST_FETCHED = 'BLOG_LIST_FETCHED';
 
 export async function fetchBlogList({ dispatch, limit = 30, offset }) {
-  const blogs = await fetch(`/api/blogs`, {
-    limit,
-    offset,
+  const blogs = (
+    await fetch(`/api/blogs`, {
+      limit,
+      offset,
+    })
+  ).map((blog) => {
+    blog.image = `/img/?q=${blog.image}`;
   });
 
   dispatch({
