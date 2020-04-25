960d2faac784082bcc82215a53ec51cee01fe622
sapphi-red <green@sapphi.red>
2020-04-25 17:03:28 +0900

    remove mock

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import { render } from './foundation/render';
-import { setupMockAPIData } from './foundation/gateway';
 import './foundation/styles';
 
 function init() {
-  if (process.env.USE_MOCK_DATA === 'true') {
-    setupMockAPIData();
-  }
-
   render();
 }
 
